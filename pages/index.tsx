65021e90fb06faad5145e474c749eecd6db2f910
Avetis90 <avetis.ananyan@gmail.com>
2022-05-20 12:09:19 +0400

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Societal</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>Societal</main>
+    </div>
+  );
+};
+
+export default Home;
