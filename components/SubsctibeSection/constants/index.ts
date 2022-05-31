b2464c03b010904ae1804da3f702139aab2ebd33
Denis Krasovskiy <denis.krasovskiy@magicpowered.io>
2022-05-31 20:13:19 +0300

    feat: update subscribe form

---
+++ components/SubsctibeSection/constants/index.ts
@@ -0,0 +1,10 @@
+export const STATUS_COLORS = {
+  sending: '#5E6368',
+  error: '#EB5757',
+  success: '#27AE60'
+};
+
+const u = '375da0a534bee5c9f50a82382';
+const id = '4075512275';
+
+export const POST_URL = `https://gmail.us6.list-manage.com/subscribe/post?u=${u}&id=${id}`;
---
+++ components/UI-kit/Input/Input.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import classNames from 'classnames';
+import styles from './Input.module.scss';
+import { Typography } from '../Typography';
+
+export type InputProps = React.DetailedHTMLProps<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+> & {
+  className?: string;
+  helperText?: string;
+  helperTextColor?: string;
+};
+
+const Input: React.FC<InputProps> = ({
+  className,
+  helperText,
+  helperTextColor,
+  ...rest
+}) => {
+  return (
+    <div className={classNames(styles.root, className)}>
+      <input className={styles.input} {...rest} />
+      {helperText && (
+        <Typography variant="caption2" style={{ color: helperTextColor }}>
+          {helperText}
+        </Typography>
+      )}
+    </div>
+  );
+};
+
+export default Input;
