c2457398f3a0c0b57ac75b0d68a47a33584114f5
Maksym Khaiuk <maksym.khaiuk@magicpowered.com>
2023-01-08 12:26:56 +0200

    feat: benefits page

---
+++ constants/content-benefits-page.ts
@@ -0,0 +1,56 @@
+export const UPPER_MARQUEE_DATA = [
+  'On-chain Governance',
+  'SaaS pricing',
+  'DAO Creation',
+  'Treasury Management',
+  'Branched Multisig Wallets',
+  'Token Gated Community',
+  'ERC-20 Contract Import',
+  'Legal Entity Creation',
+  'Cross-chain Integrations',
+  'Advanced Governance Systems',
+  'Explore Page',
+  'DAO DApp Store',
+  'Reputation Systems',
+  'Token Creation'
+];
+export const LOWER_MARQUEE_DATA = [
+  'Legal Entity Creation',
+  'Cross-chain Integrations',
+  'Advanced Governance Systems',
+  'Explore Page',
+  'DAO DApp Store',
+  'Reputation Systems',
+  'Token Creation',
+  'On-chain Governance',
+  'SaaS pricing',
+  'DAO Creation',
+  'Treasury Management',
+  'Branched Multisig Wallets',
+  'Token Gated Community',
+  'ERC-20 Contract Import'
+];
+
+export const BENEFITS_CARDS = [
+  {
+    title: 'Governance. On-chain.',
+    description:
+      'Who said DAO decision-making needs to be executed off-chain? We’re bringing advanced governance systems - pioneered by Polkadot - to all DAOs.',
+    img: '/images/product-page-hero-image.png',
+    imgPsn: 'left'
+  },
+  {
+    title: 'Cross-chain by Default.',
+    description:
+      'DAOs on Societal will benefit from trustless access to the best available DeFi, privacy, identity, and social primitives via Polkadot’s XCM protocol.',
+    img: '/images/product-page-hero-image.png',
+    imgPsn: 'right'
+  },
+  {
+    title: 'Streamlined Experiences.',
+    description:
+      'We hate paying per transaction as much as anyone. Societal is a SaaS productivity platform, paid by the DAO treasury. All member transactions are gas free.',
+    img: '/images/product-page-hero-image.png',
+    imgPsn: 'left'
+  }
+];
---
+++ pages/benefits/index.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import Marquee from 'react-fast-marquee';
+
+import MainLayout from 'components/MainLayout';
+import { Typography } from 'components/UI-kit/Typography';
+import {
+  BENEFITS_CARDS,
+  LOWER_MARQUEE_DATA,
+  UPPER_MARQUEE_DATA
+} from 'constants/content-benefits-page';
+import CardBenefitsPage from 'components/CardBenefitsPage';
+
+import styles from './BenefitsPage.module.scss';
+
+const BenefitsPage = () => {
+  return (
+    <MainLayout title="Benefits | Societal">
+      <div className={styles.slide}>
+        <Typography
+          variant="display1"
+          className={styles.mainHeading}
+          data-text="Bring on the Appchain Future."
+        >
+          Bring on the Appchain Future.
+        </Typography>
+        <Typography variant="body1" className={styles.appchainDescription}>
+          Application-specific blockchains open up a world of new design space.
+          Societal is purpose-built to service DAO creation and management,
+          securely interconnected to many other web3 networks.
+        </Typography>
+      </div>
+      <div className={styles.marqueeWrapper}>
+        <Marquee
+          className={styles.marquee}
+          gradient={false}
+          direction="right"
+          pauseOnHover
+        >
+          <div className={styles.chipsWrapper}>
+            {UPPER_MARQUEE_DATA.map(el => (
+              <div key={el} className={styles.chip}>
+                <Typography variant="title3">{el}</Typography>
+              </div>
+            ))}
+          </div>
+        </Marquee>
+        <Marquee
+          gradient={false}
+          direction="right"
+          pauseOnHover
+          className={styles.marquee}
+        >
+          <div className={styles.chipsWrapper}>
+            {LOWER_MARQUEE_DATA.map(el => (
+              <div key={el} className={styles.chip}>
+                <Typography variant="title3">{el}</Typography>
+              </div>
+            ))}
+          </div>
+        </Marquee>
+      </div>
+      <div className={styles.cardsWrapper}>
+        {BENEFITS_CARDS.map(card => (
+          <CardBenefitsPage
+            {...card}
+            className={styles.card}
+            key={card.title}
+          />
+        ))}
+      </div>
+    </MainLayout>
+  );
+};
+
+export default BenefitsPage;
