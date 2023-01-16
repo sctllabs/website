3638c150280d36dd9649fce03200a248d1fe296a
Maksym Khaiuk <maksym.khaiuk@magicpowered.com>
2023-01-16 12:08:16 +0200

    fix: update benefits page content

--- constants/content-benefits-page.ts
+++ constants/content-benefits-page.ts
@@ -43,7 +43,7 @@ export const BENEFITS_CARDS = [
     title: 'Cross-chain by Default.',
     description:
       'DAOs on Societal will benefit from trustless access to the best available DeFi, privacy, identity, and social primitives via cross-chain communication.',
-    img: '/images/benefits-page-crosschain.png',
+    img: '/images/benefits-page-cross-chain.png',
     imgPsn: 'right'
   },
   {
@@ -52,5 +52,12 @@ export const BENEFITS_CARDS = [
       'We hate paying per transaction as much as anyone. Societal is a SaaS productivity platform, paid by the DAO treasury. All member transactions are gas free.',
     img: '/images/benefits-page-gas-free.png',
     imgPsn: 'left'
+  },
+  {
+    title: 'Lorem ipsum dolor sit amet',
+    description:
+      'Lorem ipsum dolor sit amet consectetur. Ut sed nunc orci ante pellentesque suscipit placerat. Aliquam eget tincidunt et velit nulla. Quisque dolor semper sed orci pellentesque. Justo dui quam ut mollis.',
+    img: '/images/benefits-page-temp.png',
+    imgPsn: 'right'
   }
 ];
