eca9b78ad9f6775a817fae943a15379d8c1d722a
Maksym Khaiuk <maksym.khaiuk@magicpowered.com>
2023-02-22 13:40:54 +0200

    feat: update landing

--- pages/Home/Home.tsx
+++ pages/Home/Home.tsx
@@ -2,34 +2,33 @@ import React from 'react';
 import Image from 'next/image';
 
 import MainLayout from 'components/MainLayout';
-import BannerTitle from 'components/BannerTitle';
-
 import { Typography } from 'components/UI-kit/Typography';
-import { BANNER_TITLES, CARDS_HOME_PAGE } from 'constants/content-home-page';
+import { CARDS_HOME_PAGE } from 'constants/content-home-page';
 import CardHomePage from 'components/CardHomePage';
 import { SubscribeSection } from 'components/SubscribeSection';
+import GlitchTitle from 'components/GlitchTitle';
 
 import styles from './Home.module.scss';
 
 const Home = () => {
   return (
     <MainLayout className={styles.layout} title="Societal">
-      <BannerTitle titles={BANNER_TITLES} className={styles.bannerTitle} />
+      <GlitchTitle className={styles.bannerTitle} />
       <div className={styles.heroImage}>
         <Image
           src="/images/home-page-illustration.png"
           layout="responsive"
           width={2880}
           height={2286}
-          quality={100}
+          quality={90}
           objectFit="contain"
           priority
         />
       </div>
       <div className={styles.contentContainer}>
         <Typography variant="h2" className={styles.title}>
-          Societal is an application-specific blockchain, designed for the
-          creation and management of DAOs
+          Societal is the easiest place to organize, govern, and grow on-chain
+          communities.
         </Typography>
         <div className={styles.cardsContainer}>
           {CARDS_HOME_PAGE.map(card => (
